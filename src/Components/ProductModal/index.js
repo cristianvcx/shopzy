c14cb64f442da0bb0cae9d2b161156e7e5aa4a02
Cristian Andres <cristianvcx188@gmail.com>
2024-11-11 22:17:00 -0300

    actualizacion

--- src/Components/ProductModal/index.js
+++ src/Components/ProductModal/index.js
@@ -1,7 +1,40 @@
 import { Button, Dialog } from "@mui/material";
 import { MdClose } from "react-icons/md";
+import Rating from "@mui/material/Rating";
+import Slider from "react-slick";
+import { useRef } from "react";
+import InnerImageZoom from "react-inner-image-zoom";
+import "react-inner-image-zoom/lib/InnerImageZoom/styles.css";
 
 const ProductModal = (props) => {
+  const zoomSliderBig = useRef();
+  const zoomSlider = useRef();
+
+  var settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    fade: false,
+    arrows: true,
+  };
+
+  var settings2 = {
+    dots: false,
+    infinite: false,
+    speed: 700,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: false,
+    arrows: false,
+  };
+
+  const goto = (index) => {
+    zoomSlider.current.slickGoTo(index);
+    zoomSliderBig.current.slickGoTo(index);
+  };
+
   return (
     <>
       <Dialog
@@ -11,13 +44,95 @@ const ProductModal = (props) => {
         <Button className="close_" onClick={() => props.closeProductModal()}>
           <MdClose />
         </Button>
-        <h4 className="mb-0">
-          Albóndigas de pollo al estilo italiano totalmente naturales
+        <h4 className="mb-1 font-weight-bold">
+          Albóndigas de pollo al estilo italiano
         </h4>
         <div className="d-flex align-items-center">
-          <div className="d-flex align-items-center">
+          <div className="d-flex align-items-center mr-4">
             <span>Marcas:</span>
-            <span className="ml-2">De Welch </span>
+            <span className="ml-2">
+              <b>De Welch</b>{" "}
+            </span>
+          </div>
+          <Rating
+            name="read-only"
+            value={5}
+            size="small"
+            precision={0.5}
+            readOnly
+          />
+        </div>
+        <hr />
+        <div className="row mt-2 productDetaileModal">
+          <div className="col-md-5">
+            <div className="productZoom">
+              <Slider
+                {...settings2}
+                className="zoomSliderBig"
+                ref={zoomSliderBig}>
+                <div className="item">
+                  <InnerImageZoom
+                    zoomType="hover"
+                    zoomScale={1}
+                    src={`https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-62.jpg`}
+                  />
+                </div>
+                <div className="item">
+                  <InnerImageZoom
+                    zoomType="hover"
+                    zoomScale={1}
+                    src={`https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image2-47.jpg`}
+                  />
+                </div>
+                <div className="item">
+                  <InnerImageZoom
+                    zoomType="hover"
+                    zoomScale={1}
+                    src={`https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image3-35.jpg`}
+                  />
+                </div>
+              </Slider>
+            </div>
+            <Slider {...settings} className="zoomSlider" ref={zoomSlider}>
+              <div className="item">
+                <img
+                  src={`https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-62.jpg`}
+                  className="w-100"
+                  onClick={() => goto(0)}
+                />
+              </div>
+              <div className="item">
+                <img
+                  src={`https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image2-47.jpg`}
+                  className="w-100"
+                  onClick={() => goto(1)}
+                />
+              </div>
+              <div className="item">
+                <img
+                  src={`https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image3-35.jpg`}
+                  className="w-100"
+                  onClick={() => goto(2)}
+                />
+              </div>
+            </Slider>
+          </div>
+          <div className="col-md-7">
+            <div className="d-flex info align-items-center mb-3">
+              <span className="oldPrice lg mr-2">$9.35</span>
+              <span className="netPrice text-danger lg">$7.25</span>
+            </div>
+
+            <span className="badge bg-success">EN STOCK</span>
+            <p className="mt-3">
+              Vivamus adipiscing nisl ut dolor digissim semper. Nulla luctus
+              malesuada tincidunt. Class aptent taciti sociosqu ad litora
+              torquent
+            </p>
+            <div className="d-flex align-items-center">
+              <div className="quantityDrop d-flex align-items-center"></div>
+              <Button className="btn-blue btn-lg btn-big btn-round">Añadir al Carrito</Button>
+            </div>
           </div>
         </div>
       </Dialog>
